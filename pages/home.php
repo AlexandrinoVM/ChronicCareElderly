6b2a132c52370e744f6e64581e97d20a0ae4d8d3
DevBarbosa <104536028+mateus-barbosa@users.noreply.github.com>
2024-05-21 21:26:15 -0300

    Ajustes gerais

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -6,6 +8,7 @@
     include_once("../components/head.php");
     echo head("CHRONIC CARE ELDERLY");
   ?>
+  <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
 
@@ -19,10 +22,20 @@
       <div class="container">
         <h2><?= $mensagem["titulo_principal"] ?></h2>
         <p> <?= $mensagem["apresentacao"] ?> </p>
-        <a href="#" class="btn"><?= $mensagem["ver_materia"] ?> </a>
       </div>
     </section>
   </main>
+
+  <section class="articles">
+    <article>
+      <h3>Nome do Artigo</h3>
+      <h4>2024-05-21 By Barbosa</h4>
+      <p>What is Lorem? <br>
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Quos eos alias dolores nulla a eum molestias laboriosam maiores 
+      animi nostrum iure magni iste recusandae, ipsam rerum suscipit ut itaque praesentium."</p>
+    </article>
+  </section>
 
   <?php
     include_once("../components/footer.php");
