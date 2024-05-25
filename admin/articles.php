09b37de488fb94a615d6fe17a4a87836f7820650
alexandrinoVM <vinicius.moraes2@outlook.com>
2024-05-25 19:39:50 -0300

    artigos ordenados por id

--- admin/articles.php
+++ admin/articles.php
@@ -1,6 +1,8 @@
 <?php
 
 $artigo = array();
+$artigo_content = array();
+
 
 $artigo[] = array(
   'lang' => array('pt' => array(
@@ -19,5 +21,30 @@ $artigo[] = array(
     'titulo' => 'Mental Health on Elden Ring',
     'artigo' => 'So much things that I want to do before I die')),
 );
+
+$artigo_content[] = array(
+  
+    'id' => 0, 
+    
+    'titulo' => 'Como Cuidar da Saúde Física dos Idosos',
+    'conteudo' => ' O envelhecimento traz várias mudanças físicas, e é essencial entender como cuidar da saúde física dos idosos para garantir uma boa qualidade de vida.<br>
+    Alimentação Balanceada: Importância de uma dieta rica em nutrientes e exemplos de refeições saudáveis.<br>
+    Exercícios Físicos: Benefícios de atividades físicas regulares e recomendações de exercícios adequados.<br>
+    Visitas Regulares ao Médico: Importância dos check-ups regulares e monitoramento de condições crônicas.<br>
+    Prevenção de Quedas: Medidas para evitar quedas e acidentes domésticos.<br>'
+    
+
+);
+
+$artigo_content[] = array(
+  
+    'id' => 1, 
+    'titulo' => 'teste',
+    'conteudo' => ' Idosos utilizam os serviços de saúde – nos níveis primário, secundário e terciário – com maior frequência em virtude da maior incidência de doenças crônicas na terceira idade. As doenças crônicas, que podem ser múltiplas, necessitam de acompanhamento e exames frequentes, além de cuidados e medicação contínuos. ',
+);
+
+
+
+
 
 $simples = 'asldkmalskdmalksmdaklsdm';
--- pages/post.php
+++ pages/post.php
@@ -5,9 +5,10 @@
   include_once("../components/header.php");
   include_once("../components/article.php");
   include_once("../components/footer.php");
+  include_once("../admin/articles.php");
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html >
 <html lang="pt">
 <head>
   <?php
@@ -16,13 +17,12 @@
   <link rel="stylesheet" href="../css/post.css">
 </head>
 <body>
-  <h1>Como Cuidar da Saúde Física dos Idosos</h1>
-  <p>Introdução: O envelhecimento traz várias mudanças físicas, e é essencial entender como cuidar da saúde física dos idosos para garantir uma boa qualidade de vida. </p>
-  <p>Seções:</p>
-  <p>Alimentação Balanceada: Importância de uma dieta rica em nutrientes e exemplos de refeições saudáveis.</p>
-  <p>Exercícios Físicos: Benefícios de atividades físicas regulares e recomendações de exercícios adequados.</p>
-  <p>Visitas Regulares ao Médico: Importância dos check-ups regulares e monitoramento de condições crônicas.</p>
-  <p>Prevenção de Quedas: Medidas para evitar quedas e acidentes domésticos.</p>
+  <?php 
+    $id = $_GET['id'];
+  ?>
+
+  <h1><?= $artigo_content[$id]['titulo']?></h1>
+  <p><?= $artigo_content[$id]['conteudo']?></p>
   
 </body>
 </html>
