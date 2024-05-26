6a089ef5d82e816a018085dbc93972a83adf9274
DevBarbosa <mateusjbarbosa00@gmail.com>
2024-05-26 18:44:54 -0300

    .

--- components/header.php
+++ components/header.php
@@ -5,9 +5,8 @@ function headers() {
       <div class="container">
         <h1>Sênior com Saúde</h1>
         <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-          </ul>
+          <a href="#">Add Post</a>
+          <a href="#">Login / Register</a>
         </nav>
       </div>
     </header>
